ca59c2f56b72db36d64188acadf9d51e236011f1
juliomc23 <julito2302mc@gmail.com>
2022-05-19 19:08:09 +0200

    html y css structure

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./src/css/main.css" type="text/css" rel="stylesheet">
+    <title>Login System PHP</title>
+</head>
+
+<body>
+    <main class="main__container">
+        <header class="header">
+            <h1 class="header__h1">Login System with PHP</h1>
+            <div>
+                <button class="header__button--login"><a href="./modules/login.php">Login</a></button>
+            </div>
+        </header>
+        <section class="main__section">
+            <div class="articles__div">
+                <article class="section__article">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, illo accusantium veritatis repellendus alias ipsam voluptatum repudiandae porro? Optio, veritatis nobis. Impedit minus voluptates pariatur, omnis suscipit itaque libero facere.
+                    <button class="article__button__see">See more</button>
+                </article>
+                <article class="section__article">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, illo accusantium veritatis repellendus alias ipsam voluptatum repudiandae porro? Optio, veritatis nobis. Impedit minus voluptates pariatur, omnis suscipit itaque libero facere.
+                    <button class="article__button__see">See more</button>
+                </article>
+                <article class="section__article">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, illo accusantium veritatis repellendus alias ipsam voluptatum repudiandae porro? Optio, veritatis nobis. Impedit minus voluptates pariatur, omnis suscipit itaque libero facere.
+                    <button class="article__button__see">See more</button>
+                </article>
+            </div>
+            <aside class="aside">
+                <navbar class="aside__navbar">
+                    <ul class="navbar__list">
+                        <li class="navbar__elements">Lorem ipsum</li>
+                        <li class="navbar__elements">Lorem ipsum</li>
+                        <li class="navbar__elements">Lorem ipsum</li>
+                        <li class="navbar__elements">Lorem ipsum</li>
+                    </ul>
+                </navbar>
+            </aside>
+        </section>
+        <footer class="footer">
+            <div class="footer__div">
+                <span class="footer__title">Lorem ipsum dolor</span>
+                <ul class="footer__list">
+                    <li class="ul__liElements">Lorem </li>
+                    <li class="ul__liElements">Lorem </li>
+                </ul>
+            </div>
+            <div class="footer__div">
+                <span class="footer__title">Lorem ipsum dolor</span>
+                <ul class="footer__list">
+                    <li class="ul__liElements">Lorem </li>
+                    <li class="ul__liElements">Lorem </li>
+                    <li class="ul__liElements">Lorem </li>
+                </ul>
+            </div>
+        </footer>
+    </main>
+</body>
+
+</html>
