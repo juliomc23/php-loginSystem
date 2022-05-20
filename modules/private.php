d729c90d8cf4363a44b6ec72f387224edacb1acb
juliomc23 <julito2302mc@gmail.com>
2022-05-20 14:01:13 +0200

    added img

--- modules/private.php
+++ modules/private.php
@@ -10,7 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../src/css/main.css" type="text/css" rel="stylesheet">
-    <title>Login System PHP</title>
+    <title>Private part PHP</title>
 </head>
 
 <body>
